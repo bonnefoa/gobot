4b0143804fe5e7e59c910a032ab39302109b6e64
Sora <anthonin.bonnefoy@gmail.com>
2013-03-24 16:03:06 +0100

    Indentation

--- src/testing/assert/assert.go
+++ src/testing/assert/assert.go
@@ -3,8 +3,8 @@ package assert
 import "testing"
 
 func AssertEquals(t *testing.T, a interface{}, b interface{}) {
-  if a != b {
-    t.Logf("Expected '%v', got '%v'\n", a, b)
-    t.Fail()
-  }
+        if a != b {
+                t.Logf("Expected '%v', got '%v'\n", a, b)
+                t.Fail()
+        }
 }
