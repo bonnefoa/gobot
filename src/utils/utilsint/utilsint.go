5f1b623f80f7ef4fc893158d6b38680cacf39b8c
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2013-07-16 18:18:10 +0200

    Added min function

--- src/utils/utilsint/utilsint.go
+++ src/utils/utilsint/utilsint.go
@@ -4,3 +4,8 @@ func MaxInt(int1, int2 int) int {
         if int1 > int2 { return int1 }
         return int2
 }
+
+func MinInt(int1, int2 int) int {
+        if int1 < int2 { return int1 }
+        return int2
+}
